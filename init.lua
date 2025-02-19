59c767cb570e9cee72f61443c1fb5bfda83fcfc4
Kyle Porter <kyle3porter@gmail.com>
2025-02-18 16:34:37 -0800

    Added vim options

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
 require("config.lazy")
 
 
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.scrolloff = 10
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
