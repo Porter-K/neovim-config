2df0a101b31c51f6b53589624ede7190a005823e
Kyle Porter <kyle3porter@gmail.com>
2025-02-19 14:29:05 -0800

    Added dashboard-nvim Most of the config is similar to lazyvims config for dashboard. Header is half copied from lazyvim.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -104,4 +104,38 @@ return {
   {
     "echasnovski/mini.nvim",
   },
+  {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+      local logo = [[
+        ███╗  ██╗██╗   ██╗██╗███╗   ███╗
+        ████╗ ██║██║   ██║██║████╗ ████║
+        ██╔██╗██║██║   ██║██║██╔████╔██║
+        ██║╚████║╚██╗ ██╔╝██║██║╚██╔╝██║
+        ██║ ╚███║ ╚████╔╝ ██║██║ ╚═╝ ██║
+        ╚═╝  ╚══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝
+      ]]
+      logo = string.rep("\n", 8) .. logo .. "\n\n"
+      require('dashboard').setup {
+        -- config
+        theme = "doom",
+        config = {
+          header = vim.split(logo, "\n"), --your header
+          center = {
+            { action = "Telescope find_files",                           desc = " Find File",       icon = " ", key = "f" },
+            { action = "ene | startinsert",                              desc = " New File",        icon = " ", key = "n" },
+            { action = "Lazy",                                           desc = " Lazy",            icon = "󰒲 ", key = "l" },
+            { action = function() vim.api.nvim_input("<cmd>qa<cr>") end, desc = " Quit",            icon = " ", key = "q" },
+          },
+          footer = function()
+            local stats = require("lazy").stats()
+            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+            return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+          end,
+        },
+      }
+    end,
+    dependencies = { {'nvim-tree/nvim-web-devicons'}}
+  }
 }
