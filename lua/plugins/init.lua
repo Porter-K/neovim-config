c3360ca93c4e5667ad7488a44875162334f2e98d
Kyle Porter <kyle3porter@gmail.com>
2025-02-19 15:41:04 -0800

    Added persistence.nvim Moved dashboard-nvim to its own file.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -105,37 +105,16 @@ return {
     "echasnovski/mini.nvim",
   },
   {
-    'nvimdev/dashboard-nvim',
-    event = 'VimEnter',
-    config = function()
-      local logo = [[
-        ███╗  ██╗██╗   ██╗██╗███╗   ███╗
-        ████╗ ██║██║   ██║██║████╗ ████║
-        ██╔██╗██║██║   ██║██║██╔████╔██║
-        ██║╚████║╚██╗ ██╔╝██║██║╚██╔╝██║
-        ██║ ╚███║ ╚████╔╝ ██║██║ ╚═╝ ██║
-        ╚═╝  ╚══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝
-      ]]
-      logo = string.rep("\n", 8) .. logo .. "\n\n"
-      require('dashboard').setup {
-        -- config
-        theme = "doom",
-        config = {
-          header = vim.split(logo, "\n"), --your header
-          center = {
-            { action = "Telescope find_files",                           desc = " Find File",       icon = " ", key = "f" },
-            { action = "ene | startinsert",                              desc = " New File",        icon = " ", key = "n" },
-            { action = "Lazy",                                           desc = " Lazy",            icon = "󰒲 ", key = "l" },
-            { action = function() vim.api.nvim_input("<cmd>qa<cr>") end, desc = " Quit",            icon = " ", key = "q" },
-          },
-          footer = function()
-            local stats = require("lazy").stats()
-            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-            return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
-          end,
-        },
-      }
-    end,
-    dependencies = { {'nvim-tree/nvim-web-devicons'}}
+    "folke/persistence.nvim",
+    event = "BufReadPre", -- this will only start session saving when an actual file was opened
+    opts = {
+      -- add any custom options here
+    },
+    keys = {
+      {"n", "<leader>qs", function() require("persistence").load() end, desc = "Load current directory session",},
+	    {"n", "<leader>qS", function() require("persistence").select() end, desc = "Select session",},
+	    {"n", "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Load last session",},
+      {"n", "<leader>qd", function() require("persistence").stop() end, desc = "Stop persistence, don't save session on exit",},
+    },
   }
 }
