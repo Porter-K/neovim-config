582b220137a3390d7027b8d4bcbfaf76fd638358
Kyle Porter <kyle3porter@gmail.com>
2025-02-18 23:50:31 -0800

    Added COQ_nvim for code completion

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -20,5 +20,18 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
+    lazy = false,
+    dependencies = {
+      { "ms-jpq/coq_nvim", branch = "coq" },
+      { "ms-jpq/coq.artifacts", branch = "artifacts" },
+    },
+    init = function()
+      vim.g.coq_settings = {
+        auto_start = true,
+      }
+    end,
+  },
+  {
+    "ms-jpq/coq_nvim",
   },
 }
