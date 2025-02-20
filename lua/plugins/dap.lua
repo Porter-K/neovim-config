970c79d96b43d69187de865a7d53e59c0022cd31
Kyle Porter <kyle3porter@gmail.com>
2025-02-19 21:59:11 -0800

    Added debugging with nvim-dap, mason-dap-adapter and nvim-dap-ui

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
+    },
+    config = function()
+      require("dapui").setup()
+      local dap, dapui = require("dap"), require("dapui")
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+    end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    dependencies = {
+      "mwilliamboman/mason.nvim",
+    },
+    config = function()
+      require("mason-nvim-dap").setup()
+      require("mason-nvim-dap").setup({
+        ensure_installed = { "firefox-debug-adapter" },
+        handlers = {},
+      })
+    end,
+  },
+}
