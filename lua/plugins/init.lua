86f5709ab2ff8aa6d51f86a878bca3b363cb4ed5
Kyle Porter <kyle3porter@gmail.com>
2025-02-19 20:13:32 -0800

    Added FTerm.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -62,4 +62,11 @@ return {
   {
     "tpope/vim-fugitive",
   },
+  {
+    "numToStr/FTerm.nvim",
+    config = function()
+      vim.keymap.set('n', '<A-i>', '<CMD>lua require("FTerm").toggle()<CR>')
+      vim.keymap.set('t', '<A-i>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+    end,
+  },
 }
