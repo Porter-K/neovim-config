f336e8edc44722fd042d2972ece632f3e5f06aa7
Kyle Porter <kyle3porter@gmail.com>
2025-03-17 15:16:47 -0700

    Added some more plugins Added noice nvim with nvim-notify and nui.nvim Added trouble.nvim

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -34,7 +34,7 @@ return {
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
 					{ name = "vsnip" }, -- For vsnip users.
-					-- { name = 'luasnip' }, -- For luasnip users.
+					{ name = "luasnip" }, -- For luasnip users.
 					-- { name = 'ultisnips' }, -- For ultisnips users.
 					-- { name = 'snippy' }, -- For snippy users.
 				}, {
@@ -76,9 +76,9 @@ return {
 			-- Set up lspconfig.
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-			require("lspconfig")["<YOUR_LSP_SERVER>"].setup({
-				capabilities = capabilities,
-			})
+			--require("lspconfig")["<YOUR_LSP_SERVER>"].setup({
+				--capabilities = capabilities,
+			--})
 		end,
 	},
 }
