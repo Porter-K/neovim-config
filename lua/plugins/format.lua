0956752a2335fb3b7d7a8b328cf90cf64e6a25f7
Kyle Porter <kyle3porter@gmail.com>
2025-03-16 18:39:15 -0700

    Changed formatting config

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -2,58 +2,22 @@ return {
 	{
 		"mhartington/formatter.nvim",
 		config = function()
-			-- Utilities for creating configurations
-			local util = require("formatter.util")
 			-- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
 			require("formatter").setup({
-				-- Enable or disable logging
-				logging = true,
-				-- Set the log level
-				log_level = vim.log.levels.WARN,
-				-- All formatter configurations are opt-in
+				logging = true, -- Enable or disable logging
+				log_level = vim.log.levels.WARN,  -- Set the log level
 				filetype = {
-					-- Formatter configurations for filetype "lua" go here
-					-- and will be executed in order
 					lua = {
-						-- "formatter.filetypes.lua" defines default configurations for the
-						-- "lua" filetype
 						require("formatter.filetypes.lua").stylua,
-
-						-- You can also define your own configuration
-						function()
-							-- Supports conditional formatting
-							if util.get_current_buffer_file_name() == "special.lua" then
-								return nil
-							end
-							-- Full specification of configurations is down below and in Vim help
-							-- files
-							return {
-								exe = "stylua",
-								args = {
-									"--search-parent-directories",
-									"--stdin-filepath",
-									util.escape_path(util.get_current_buffer_file_path()),
-									"--",
-									"-",
-								},
-								stdin = true,
-							}
-						end,
 					},
-          typescript = {
-            require("formatter.filetypes.typescript").prettierd,
-          },
-          java = {
-            require("formatter.filetypes.java").google_java_format,
-          },
-					-- Use the special "*" filetype for defining formatter configurations on
-					-- any filetype
+					typescript = {
+						require("formatter.filetypes.typescript").prettierd,
+					},
+					java = {
+						require("formatter.filetypes.java").google_java_format,
+					},
 					["*"] = {
-						-- "formatter.filetypes.any" defines default configurations for any
-						-- filetype
 						require("formatter.filetypes.any").remove_trailing_whitespace,
-						-- Remove trailing whitespace without 'sed'
-						-- require("formatter.filetypes.any").substitute_trailing_whitespace,
 					},
 				},
 			})
